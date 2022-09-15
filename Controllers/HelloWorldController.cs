aa745325bbc52e6dd0525b8c737d9f432f18555d
Alejandro Santiago <cubanfireball81@gmail.com>
2022-09-14 19:04:14 -0800

    Adds new View, Model, and Updates Controller to create integrated flow of data from View to Controller.

--- Controllers/HelloWorldController.cs
+++ Controllers/HelloWorldController.cs
@@ -8,17 +8,20 @@ namespace MvcMovie.Controllers
         // 
         // GET: /HelloWorld/
 
-        public string Index()
+        public IActionResult Index()
         {
-            return "This is my default action...";
+            return View();
         }
 
         // 
         // GET: /HelloWorld/Welcome/ 
 
-        public string Welcome(string name, int ID = 1)
+        public IActionResult Welcome(string name, int numTimes = 1)
         {
-            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+            ViewData["Message"] = "Hello" + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
         }
     }
 }
